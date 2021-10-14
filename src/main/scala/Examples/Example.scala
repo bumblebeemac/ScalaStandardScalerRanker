fc42871f013f1e752f5c36592c23a7443b2f51bc
Ravi Subramanian <ravishankars8668@gmail.com>
2021-10-14 17:40:43 -0500

    Example.scala works with Kaggle data.

--- src/main/scala/Examples/Example.scala
+++ src/main/scala/Examples/Example.scala
@@ -2,7 +2,6 @@ package Examples
 
 import StandardScalerRanker.getStandardRankDF
 import org.apache.spark.sql.{SaveMode, SparkSession}
-import org.apache.spark.sql.functions.{col, concat_ws}
 
 object Example extends App {
 
@@ -11,27 +10,22 @@ object Example extends App {
     .master("local")
     .getOrCreate()
 
+  // Set in order to make sure Exception does not occur due to string concatenation
   spark.conf.set("spark.debug.maxToStringFields", 1000)
 
-//  val aDF = spark.read
-//    .format("csv")
-//    .option("header", "true")
-//    .option("sep", ",")
-//    .csv("/Users/bumblebeemac/Documents/usedcars_dataset.csv")
+  val aDF = spark.read
+    .format("csv")
+    .option("header", "true")
+    .option("sep", ",")
+    .csv("src/data/usedcars_dataset.csv")
 
-  val aSeq = Seq(
-    ("aa", "A", 5.7, 2.1),
-    ("aa", "B", 9.7, 7.4),
-    ("aa", "C", 8.1, 5.9),
-    ("bb", "X", 4.3, 1.1),
-    ("bb", "Y", 2.3, 6.8),
-    ("aa", "D", 6.6, 9.1)
-  )
-
-  import spark.implicits._
-  val aDF = aSeq.toDF("A", "B", "C", "D")
-
-  val getMeanStd = getStandardRankDF(aDF, grpByList = List("A"), stdFactors = List("C", "D"))
+  val getMeanStd = getStandardRankDF(aDF, grpByList = List("make", "aspiration", "num_of_doors", "body_style"),
+    stdFactors = List("compression_ratio", "horsepower", "peak_rpm", "city_mpg", "highway_mpg", "price"))
   getMeanStd.show()
+  getMeanStd.coalesce(1).write
+    .option("header", "true")
+    .option("sep", ",")
+    .mode(SaveMode.Overwrite)
+    .csv("src/data/getMeanStdResult.csv")
 
 }
